21177c3d1ca05dd72d88cfff58eb67375d01a904
Siyi Chen <siyich@umich.edu>
2023-02-03 00:11:25 -0500

    update sh scripts

---
+++ ssl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SSLEPOCH=100
+
+# # ssl, model 1
+# python ssl_learning.py --useout --epochs $SSLEPOCH --model 1
+# ssl, model 2
+python ssl_learning.py --useout --epochs $SSLEPOCH --model 2 --pretrain \ 
+checkpoints/ucf240_split0_2layer_2dGRU_static_B1_uoTrue_lr0.0001_wd1e-05_bs32/epoch10.pth.tar \ 
+--start-epoch 10
+# ssl, model 3
+python ssl_learning.py --useout --epochs $SSLEPOCH --model 3 --pretrain \ 
+checkpoints/ucf240_split0_2layer_2dGRU_static_B2_uoTrue_lr0.0001_wd1e-05_bs32/epoch10.pth.tar \ 
+--start-epoch 10
+# # ssl, model 4
+# python ssl_learning.py --useout --epochs $SSLEPOCH --model 4 --useout
