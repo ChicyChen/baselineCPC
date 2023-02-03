0fce96d0e2e9174c80b8c06f2e54271b1173b4ca
Siyi Chen <siyich@umich.edu>
2023-02-03 02:12:12 -0500

    update two bash scripts

--- action.sh
+++ action.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # define variables
-bbfoler="checkpoints/ucf240_split0_2layer_2dGRU_static_B1_uoTrue_lr0.0001_wd1e-05_bs32"
-echo $bbfoler
+bbfoler="checkpoints/ucf240_split0_2layer_2dGRU_static_B2_uoTrue_lr0.0001_wd1e-05_bs32"
 # bbfolder_nossl = "checkpoints/ucf240_split0_1layer_2dGRU_static_nossl"
-((bbepoch=100))
-((ftepoch=10))
+bbepoch=100
+ftepoch=10
 
 # finetune after ssl, hmdb
 python action_recognation.py --backbone_folder $bbfoler --backbone_epoch $bbepoch --epochs $ftepoch
--- ssl.sh
+++ ssl.sh
@@ -5,6 +5,10 @@ SSLEPOCH=10
 python ssl_learning.py --epochs $SSLEPOCH --model 2 --seeall
 # 2layer_2dGRU_static_B2
 python ssl_learning.py --epochs $SSLEPOCH --model 3 --seeall
+# 2layer_2dGRU_static_A1
+python ssl_learning.py --epochs $SSLEPOCH --model 4 --seeall
+# 2layer_2dGRU_static_A2
+python ssl_learning.py --epochs $SSLEPOCH --model 5 --seeall
 
 # # ssl, model 1
 # python ssl_learning.py --epochs $SSLEPOCH --model 1
